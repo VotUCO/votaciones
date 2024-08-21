13c8c244ba1d53b0629170c282972a5fc678a10f
Jesús Escribano <jesusescribano2002@gmail.com>
2024-08-21 09:54:21 +0200

    fix: Update permission to avoid errors  🐛

--- src/shared/infrastructure/is_admin_permission.py
+++ src/shared/infrastructure/is_admin_permission.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import BasePermission
-
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import AnonymousUser
 
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.rol == "admin")
+        if isinstance(request.user, AnonymousUser):
+            return Response(status=status.HTTP_401_UNAUTHORIZED, data={"error": "No se ha proporcionado un usuario activo válido"})
+        else:
+            return bool(request.user and request.user.rol == "admin")
